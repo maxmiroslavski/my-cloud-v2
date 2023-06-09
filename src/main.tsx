119418121adcef47c784eb6f4ec5aadec1dc02df
maxmiroslavski <maksimsaeros@gmail.com>
2023-06-09 12:10:39 +0500

    add hashrouter for git deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import './index.css';
 import './tailwind.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	// <React.StrictMode>
 	<Provider store={store}>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</Provider>
-	// </React.StrictMode>
 );
